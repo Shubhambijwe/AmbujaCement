73d870b6fa566b0025afbfece89b5c0d472643c1
Shubhambijwe <125531663+Shubhambijwe@users.noreply.github.com>
2023-02-18 13:50:56 +0530

    Update TestClass.java

--- Maven_Practice_Automation/src/test/java/test/TestClass.java
+++ Maven_Practice_Automation/src/test/java/test/TestClass.java
@@ -14,6 +14,9 @@ public class TestClass {
 	public static void main(String[] args) throws InterruptedException 
 	{
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Hp\\Desktop\\NotePad\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Hp\\Desktop\\NotePad\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Hp\\Desktop\\NotePad\\chromedriver_win32\\chromedriver.exe");
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
