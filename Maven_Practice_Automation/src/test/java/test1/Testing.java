eb12554d366e828a28029c5d9898fb67a77053c3
Hp <Hp@LAPTOP-JRADBEE7>
2023-02-18 14:17:39 +0530

    New Changes

---
+++ Maven_Practice_Automation/src/test/java/test1/Testing.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Testing {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Goodnight");
+		System.out.println("GoodMorning");
+		System.out.println("Hello");
+	}
+
+}
